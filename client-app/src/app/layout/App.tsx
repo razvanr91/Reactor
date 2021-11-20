bcb33ae0962085bbfe5d1bf85c1df00a55e9ba49
razvanr91 <razvan.roman91@outlook.com>
2021-11-20 18:36:03 +0000

    Update
    Add category images create activity dashboard component, activity form component + crud functionality

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { v4 as uuid } from "uuid";
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
+	const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+	const [editMode, setEditMode] = useState(false);
 
 	useEffect(() => {
 		axios.get<Activity[]>("https://localhost:7206/api/activities").then((response) => {
@@ -13,15 +17,53 @@ function App() {
 		});
 	}, []);
 
+	function handleSelectActivity(id: string) {
+		setSelectedActivity(activities.find((activity) => activity.id === id));
+	}
+
+	function handleCancelSelectActivity() {
+		setSelectedActivity(undefined);
+	}
+
+	function handleFormOpen(id?: string) {
+		id ? handleSelectActivity(id) : handleCancelSelectActivity();
+
+		setEditMode(true);
+	}
+
+	function handleFormClose() {
+		setEditMode(false);
+	}
+
+	function handleCreateOrEditActivity(activity: Activity) {
+		activity.id
+			? setActivities([...activities.filter((x) => x.id !== activity.id), activity])
+			: setActivities([...activities, { ...activity, id: uuid() }]);
+		setEditMode(false);
+		setSelectedActivity(activity);
+	}
+
+	function handleDeleteActivity(id: string) {
+		setActivities([...activities.filter((x) => x.id !== id)]);
+	}
+
 	return (
-		<div>
-			<Navbar />
-			<List>
-				{activities.map((activity) => {
-					return <List.Item key={activity.id}>{activity.title}</List.Item>;
-				})}
-			</List>
-		</div>
+		<Fragment>
+			<Navbar openForm={handleFormOpen} />
+			<Container style={{ marginTop: "7em" }}>
+				<ActivityDashboard
+					activities={activities}
+					selectedActivity={selectedActivity}
+					selectActivity={handleSelectActivity}
+					cancelSelectActivity={handleCancelSelectActivity}
+					editMode={editMode}
+					openForm={handleFormOpen}
+					closeForm={handleFormClose}
+					createOrEdit={handleCreateOrEditActivity}
+					deleteActivity={handleDeleteActivity}
+				/>
+			</Container>
+		</Fragment>
 	);
 }
 
--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
 
-export default function Navbar() {
+interface Props {
+	openForm: () => void;
+}
+
+export default function Navbar({ openForm }: Props) {
 	return (
 		<Menu inverted fixed="top">
 			<Container>
 				<Menu.Item header>
-					<img />
+					<img src="/assets/logo.png" alt="logo" style={{ marginRight: "10px" }} />
 					Reactor
 				</Menu.Item>
 				<Menu.Item name="Activities" />
 				<Menu.Item>
-					<Button positive content="Create Activity" />
+					<Button onClick={openForm} positive content="Create Activity" />
 				</Menu.Item>
 			</Container>
 		</Menu>
