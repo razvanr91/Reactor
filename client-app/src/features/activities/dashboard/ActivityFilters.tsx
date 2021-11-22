7b9a491abefccb3b602296c2257ffee1a8ffb9e7
razvanr91 <razvan.roman91@outlook.com>
2021-11-22 12:42:12 +0000

    Create activity filter component and homepage styling

---
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import Calendar from "react-calendar";
+import { Header, Menu } from "semantic-ui-react";
+
+export default function ActivityFilters() {
+	return (
+		<Fragment>
+			<Menu vertical size="large" style={{ width: "100%", marginTop: 29 }}>
+				<Header icon="filter" attached color="teal" content="Filter" />
+				<Menu.Item content="All Activities" />
+				<Menu.Item content="I'm going" />
+				<Menu.Item content="I'm hosting" />
+			</Menu>
+			<Calendar />
+		</Fragment>
+	);
+}
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,14 +1,21 @@
+import { link } from "fs";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 export default function HomePage() {
 	return (
-		<Container style={{ martinTop: "7em" }}>
-			<h1>Home Page</h1>
-			<h3>
-				Go to <Link to="/activities">Activities</Link>
-			</h3>
-		</Container>
+		<Segment inverted textAlign="center" vertical className="masthead">
+			<Container text>
+				<Header as="h1" inverted>
+					<Image size="massive" src="/assets/logo.png" alt="Logo" style={{ marginBottom: 12 }} />
+					Reactor
+				</Header>
+				<Header as="h2" inverted content="Welcome to Reactor!" />
+				<Button as={Link} to="/activities" size="huge" inverted>
+					Take me to the activities!
+				</Button>
+			</Container>
+		</Segment>
 	);
 }
